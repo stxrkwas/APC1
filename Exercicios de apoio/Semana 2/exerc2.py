108b6aeedf77e3efddca1fa638450e0f460613b7
stxrkwas <maria.silva4476@etec.sp.gov.br>
2026-04-08 07:43:59 -0300

    Praticando os exercicios de apoio.

---
+++ Exercicios de apoio/Semana 2/exerc2.py
@@ -0,0 +1,15 @@
+# Exercício de apoio 2.1 da Semana 2
+
+# %% Problema a:
+soma = 1 + 2 + 3 + 4 + 5
+print(soma)
+
+# %% Problema b:
+sara = 23
+mark = 19
+fatima = 31
+
+media = (sara + mark + fatima) / 3
+print(media)
+
+# %% Problema c:
